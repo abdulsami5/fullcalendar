fae2d97fddf3caca1b79f98a291cbb4fd74e9ed2
Abdul Sami <abdul.sami@tintash.com>
2018-07-12 14:47:12 +0500

    calendar.js: add dayClick handler to open modal

--- calendar.js
+++ calendar.js
@@ -15,7 +15,13 @@ $(function() {
             eventLimit:true,
 
 
- 
+             dayClick: function(date, jsEvent, view) {
+   // alert('a day has been clicked!' + date.format());
+              console.log(date._d.getDate());
+              console.log(jsEvent);
+              $("#myModal").modal();
+
+  } 
 
 
      })
